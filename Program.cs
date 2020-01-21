206f043272b8de7f430bd1d0b25bbb1910e05af1
dsrp197 <53283904+dsrp197@users.noreply.github.com>
2020-01-21 09:48:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese X1: ");
+            int X1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese X2: ");
+            int X2 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese X3: ");
+            int Y1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese X4: ");
+            int Y2 = int.Parse(Console.ReadLine());
+
+            double m = ((Y2 - Y1) / (X2 - X1));
+            Console.WriteLine("La pendiente es: " + m);
+
+            double b = (Y1 - (m * X1));
+            Console.WriteLine("La pendiente es: " + b);
+
+            double d = Math.Sqrt((Math.Pow(Y2 - Y1, 2)) + (Math.Pow(X2 - X1, 2)));
+            Console.WriteLine("La distancia es: " + d);
+        }
+    }
+}
