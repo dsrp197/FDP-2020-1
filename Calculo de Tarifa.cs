52768816f9bd13b495c961a5fe0313c5e8f506f3
dsrp197 <53283904+dsrp197@users.noreply.github.com>
2020-01-30 07:14:29 -0500

    Add files via upload

---
+++ Calculo de Tarifa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_de_Tarifa
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //Ingreso de salario
+                Console.WriteLine("Ingrese su salario en cuestión de smmlvm, sabiendo que 1 smmlv = 877.803 COP");
+                int salario = int.Parse(Console.ReadLine());
+
+                //Condiciones de acuerdo a el número de smmlv que el usuario adquiere
+
+                if (salario < 2)
+                {
+                    Console.WriteLine("Tarifa A");
+                }
+
+                else if (2 <= salario && salario < 4)
+                {
+                    Console.WriteLine("Tarifa B");
+                }
+
+                else
+                {
+                    Console.WriteLine("Tarifa C");
+                }
+            }
+        }
+    }
+}
+    }
+}
