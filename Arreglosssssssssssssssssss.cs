016e4b3ecf8b9385e657e8230941e7ad613bb757
dsrp197 <53283904+dsrp197@users.noreply.github.com>
2020-02-27 09:55:55 -0500

    Add files via upload

---
+++ Arreglosssssssssssssssssss.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main()
+        {   
+            Console.Write("Ingrese el número de datos que se tienen:");
+            int n = int.Parse(Console.ReadLine());
+            int[] edades = new int[n];
+            int max = 0, min = 100;
+            double total = 0;
+            string NombreMayor = "";
+            string NombreMenor = "";
+            string PCP = "";
+            string[] nombres = new string[n];
+
+            for (int i = 0; i < edades.Length; i++)
+            {
+                Console.WriteLine("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+                edades[i] = edad;
+                total += edades[i];
+
+                if (max < edades[i]) max = edades[i];
+                if (min > edades[i]) min = edades[i];
+
+
+                Console.WriteLine("Nombre: ");
+                string nombre = (Console.ReadLine());
+                nombres[i] = nombre;
+
+                if (max == edades[i]) NombreMayor = nombres[i];
+                if (min == edades[i]) NombreMenor = nombres[i];
+
+
+            }
+
+            double promedio = total / edades.Length;
+            double[] sumatoria = new double[n];
+            double suma = 0;
+
+            for (int i = 0; i < edades.Length; i++)
+            {
+                int xi = edades[i];
+                suma += Math.Pow((xi - promedio), 2);
+            }
+
+            double desviacion = Math.Sqrt(suma / n);
+        
+            Console.WriteLine("El promedio de edades es: " + promedio);
+            Console.WriteLine("La desviación estandar es: " + desviacion);
+            Console.WriteLine("Edad Max: " + max);
+            Console.WriteLine("nombre del Myor: " + NombreMayor);
+
+            Console.WriteLine("Edad Min: " + min);
+            Console.WriteLine("nombre del Mnor: " + NombreMenor);
+
+
+
+
+
+
+        }
+    }
+}
